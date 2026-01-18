ed282fe1b79ca317d9e93aac7ef1b3aa1bd9138a
supachoke789654 <150837225+supachoke789654@users.noreply.github.com>
2026-01-19 02:46:06 +0700

    Edit Extraction Node

--- backend/agent_runner.py
+++ backend/agent_runner.py
@@ -8,9 +8,49 @@ from typing import TypedDict, List, Dict, Any
 from langgraph.graph import StateGraph, END
 from openai import OpenAI
 from dotenv import load_dotenv
+import re
 
 load_dotenv()
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+THAI_DIGIT_MAP = {
+    "ศูนย์": "0",
+    "หนึ่ง": "1", "เอ็ด": "1",
+    "สอง": "2",
+    "สาม": "3",
+    "สี่": "4",
+    "ห้า": "5",
+    "หก": "6",
+    "เจ็ด": "7",
+    "แปด": "8",
+    "เก้า": "9",
+}
+
+
+def thai_words_to_digits(text: str) -> str:
+    for word, digit in THAI_DIGIT_MAP.items():
+        text = text.replace(word, digit)
+
+    # ลบช่องว่างระหว่างตัวเลข
+    text = re.sub(r'(?<=\d)\s+(?=\d)', '', text)
+
+    return text
+
+def extract_thai_mobile_from_asr(text: str):
+    #แปลงคำอ่าน → ตัวเลข
+    normalized = thai_words_to_digits(text)
+
+    #ลบขีด/ช่องว่าง
+    normalized = re.sub(r'[\s\-]', '', normalized)
+
+    PHONE_REGEX = re.compile(
+    r'(?:\+66|66|0)(?:6|8|9)\d{8}'
+      )
+    #สกัดเบอร์
+    phones = PHONE_REGEX.findall(normalized)
+
+    return phones
+
 
 # เเปลง .webm เป็น wav เพื่อให้ gpt-4o-transcribe เเปลงจากเสียงเป็นข้อความได้
 def webm_to_wav(webm_bytes: bytes) -> bytes:
@@ -79,34 +119,88 @@ class VoiceGraph:
 
     # ใช้ LLM (gpt-5.2) สกัดคําเเละให้คะเเนนการสะกัดคํา
     async def extract_node(self, state: VoiceState):
+        
         print(state['transcript'],flush=True)
+        
+        phone_dict =   {}
+
+        gender_dict = {}
+
+        if "ชาย" in state["transcript"] or "male" in state["transcript"].lower():
+          gender_dict = {
+          "field": "เพศ",
+          "value": "ชาย",
+          "หลักฐาน": "ชาย",
+          "คะเเนนความลื่นไหลของข้อความ": 1.0,
+          "คะเเนนความต่อเนื่องของข้อความ": 1.0,
+          "คะเเนนความถูกต้องของข้อความ": 1.0
+                    }
+
+        elif "หญิง" in state["transcript"] or "female" in state["transcript"].lower():
+          gender_dict = {
+          "field": "เพศ",
+          "value": "หญิง",
+          "หลักฐาน": "หญิง",
+          "คะเเนนความลื่นไหลของข้อความ": 1.0,
+          "คะเเนนความต่อเนื่องของข้อความ": 1.0,
+          "คะเเนนความถูกต้องของข้อความ": 1.0
+                    }
+
+        else :
+
+          gender_dict = {
+          "field": "เพศ",
+          "value": None,
+          "หลักฐาน": None,
+          "คะเเนนความลื่นไหลของข้อความ": 0.0,
+          "คะเเนนความต่อเนื่องของข้อความ": 0.0,
+          "คะเเนนความถูกต้องของข้อความ": 0.0
+        }
+          
+        phone = extract_thai_mobile_from_asr(state["transcript"])
+        
+        if len(phone) != 0 :
+          phone_dict =   {
+            "field": "เบอร์โทรศัพท์",
+            "value": phone[0],
+            "หลักฐาน": phone[0],
+            "คะเเนนความลื่นไหลของข้อความ": 1.0,
+            "คะเเนนความต่อเนื่องของข้อความ": 1.0,
+            "คะเเนนความถูกต้องของข้อความ": 1.0
+          }
+          
+        else :
+          phone_dict =   {
+            "field": "เบอร์โทรศัพท์",
+            "value": "...",
+            "หลักฐาน": "...",
+            "คะเเนนความลื่นไหลของข้อความ": 0.0,
+            "คะเเนนความต่อเนื่องของข้อความ": 0.0,
+            "คะเเนนความถูกต้องของข้อความ": 0.0
+          }
+          
         prompt = f"""
 
 ให้สกัดข้อมูลต่อไปนี้จากข้อความภาษาไทยหรืออังกฤษ หากไม่พบให้ใส่ค่าเป็น null
 - ชื่อ
 - นามสกุล
-- เพศ
-- เบอร์โทรศัพท์
 - ทะเบียนรถ
 
 กติกาเพิ่มเติม:
 1. หากมีการสะกดชื่อเป็นตัวอักษร (เช่น A B C หรือ เอ บี ซี) ให้ยึดตัวอักษรที่สะกดเป็นหลัก
-2. ให้ช่วยแก้คำผิดที่เกิดจากการถอดเสียง ทั้งภาษาไทยและภาษาอังกฤษ
-3. "หลักฐาน" หมายถึง ข้อความช่วงที่ใช้ยืนยันว่าข้อมูลนั้นถูกสกัดออกมา
+2. "หลักฐาน" หมายถึง ข้อความช่วงที่ใช้ยืนยันว่าข้อมูลนั้นถูกสกัดออกมา
+3. ทะเบียนรถจะต่างจากเบอร์โทรศัพท์ตรงที่ข้อความจะมาเป็นพยัญชนะเช่น ก, กอ ก่อนเเล้วตามด้วยตัวเลขหรือในบางกรณีอาจจะขึ้นต้นด้วยตัวเลขก่อน เช่น 1กง9874 โดยตัวเลขที่ติดกันจะมีไม่เกิน 4 ตัวเลข ถ้าเกินไม่ต้องบันทึก
 4. ให้ประเมินคะแนนในช่วง 0.0 – 1.0 โดยใช้หลักเกณฑ์ดังนี้
    - คะแนนความลื่นไหลของข้อความ: หลักฐานมีคำอื่นปนหรือขาดตอนหรือไม่
    - คะแนนความต่อเนื่องของข้อความ: การพูดต่อเนื่อง ไม่เว้นช่วงนาน
    - คะแนนความถูกต้องของข้อความ: ความถูกต้องของคำหรือรูปแบบข้อมูล
-5. อย่าเดาเพศจากชื่อเเละหางเสียงครับหรือค่ะ รอให้ในข้อความมีการระบุเพศที่ชัดเจนเเล้วค่อยใส่เพศ
-6. ทะเบียนรถจะต่างจากเบอร์โทรศัพท์ตรงที่ข้อความจะมาเป็นพยัญชนะเช่น ก, กอ ก่อนเเล้วตามด้วยตัวเลขหรือในบางกรณีอาจจะขึ้นต้นด้วยตัวเลขก่อน เช่น 1กง9874 โดยตัวเลขที่ติดกันจะมีไม่เกิน 4 ตัวเลข ถ้าเกินไม่ต้องบันทึก
-7. เบอร์โทรศัพท์จะเป็นตัวเลข 10 ตัวเสมอถ้าไม่ครบหรือเกินไม่ต้องใส่ข้อมูลเบอร์โทรศัพท์
 
 รูปแบบผลลัพธ์:
 - ต้องเป็น JSON array เท่านั้น
 - ห้ามมีข้อความอธิบายเพิ่มเติมนอก JSON
 - ต้องมี object ครบทุกฟิลด์ตามโครงสร้างด้านล่าง
 
-โครงสร้าง JSON ที่ต้องส่งกลับ:
+ตัวอย่างโครงสร้าง JSON ที่ต้องส่งกลับ:
 
 [
   {{
@@ -117,7 +211,7 @@ class VoiceGraph:
     "คะเเนนความต่อเนื่องของข้อความ": 0.0,
     "คะเเนนความถูกต้องของข้อความ": 0.0
   }},
-  
+
   {{
     "field": "นามสกุล",
     "value": "...",
@@ -126,25 +220,7 @@ class VoiceGraph:
     "คะเเนนความต่อเนื่องของข้อความ": 0.0,
     "คะเเนนความถูกต้องของข้อความ": 0.0
   }},
-  
-  {{
-    "field": "เพศ",
-    "value": "...",
-    "หลักฐาน": "...",
-    "คะเเนนความลื่นไหลของข้อความ": 0.0,
-    "คะเเนนความต่อเนื่องของข้อความ": 0.0,
-    "คะเเนนความถูกต้องของข้อความ": 0.0
-  }},
-  
-  {{
-    "field": "เบอร์โทรศัพท์",
-    "value": "...",
-    "หลักฐาน": "...",
-    "คะเเนนความลื่นไหลของข้อความ": 0.0,
-    "คะเเนนความต่อเนื่องของข้อความ": 0.0,
-    "คะเเนนความถูกต้องของข้อความ": 0.0
-  }},
-  
+
   {{
     "field": "ทะเบียนรถ",
     "value": "...",
@@ -159,38 +235,32 @@ Transcript:
 {state['transcript']}
 """
 
-
         res = await asyncio.to_thread(
             self.client.chat.completions.create,
-            model="gpt-5.2",  
+            model="gpt-5.2",
             messages=[
                 {"role": "system", "content": "คุณคือระบบสำหรับสกัดข้อมูลเชิงโครงสร้างจากบทสนทนา"},
                 {"role": "user", "content": prompt}
             ]
         )
-        
+
         raw = res.choices[0].message.content.strip()
-        print(raw, flush=True)
 
         if raw.startswith("```"):
-            raw = raw.strip("`")
-            raw = raw.replace("json", "", 1).strip()
+            raw = raw.strip("`").replace("json", "").strip()
 
         try:
-            parsed = json.loads(raw)
-
-            if isinstance(parsed, list):
-                structured = parsed
-            elif isinstance(parsed, dict):
-                structured = [parsed]
-            else:
-                structured = []
-
-        except json.JSONDecodeError:
+            structured = json.loads(raw)
+        except Exception:
             structured = []
+
+        structured.append(gender_dict)
+        structured.append(phone_dict)
+        
+        print(structured, flush=True)
         
         return {"structured": structured}
-
+    
     # นําคะเเนนที่ได้จาก LLM มาเช็คว่าผ่านไหม
     def check_node(self, state: VoiceState):
         structured: List[Dict[str, Any]] = state.get("structured", [])
